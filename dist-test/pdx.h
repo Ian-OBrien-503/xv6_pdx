e93ae06bdd4c5ae76df5ab9b7b1f8733a8637ca1
Ian O'Brien <iobrien@fab09.cecs.pdx.edu>
2019-02-27 20:44:15 -0800

    ignore last commit, this is first p4 commit

---
+++ dist-test/pdx.h
@@ -0,0 +1,40 @@
+/*
+ * This file contains types and definitions for Portland State University.
+ * The contents are intended to be visible in both user and kernel space.
+ */
+
+#ifndef PDX_INCLUDE
+#define PDX_INCLUDE
+
+#define TRUE 1
+#define FALSE 0
+#define RETURN_SUCCESS 0
+#define RETURN_FAILURE -1
+#define UID 0
+#define GID 0
+#define P2TICKS 0
+// for ps.c and getprocs()
+#define MAX 72 
+#define STRSIZE 10 
+
+//for project #4
+#define MAXPRIO 5
+#define DEFAULT_BUDGET 1000
+#define TICKS_TO_PROMOTE 100
+//end add for project #4
+
+
+#define NUL 0
+#ifndef NULL
+#define NULL NUL
+#endif  // NULL
+
+#define TPS 1000   // ticks-per-second
+#define SCHED_INTERVAL (TPS/100)  // see trap.c
+
+#define NPROC  64  // maximum number of processes -- normally in param.h
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+#endif  // PDX_INCLUDE
